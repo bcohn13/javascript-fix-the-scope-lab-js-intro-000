c7ace97d562eaeb4de6bdf231e0dd5ec4c1713d0
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-17 20:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ function add2(n) {
 
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function() 
+  {
     return "FUNKY!"
   }
-}
+
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
